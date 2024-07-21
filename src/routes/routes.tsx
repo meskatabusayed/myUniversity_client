86ecb9be4bb456b53617408ffcce1d6091e6daac
meskatabusayed <meskatabusayed@gmail.com>
2024-07-22 00:37:03 +0530

    registration page create

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Registration from "../pages/Registration";
 
 const router  = createBrowserRouter([
     {
         path: '/',
-        element : <App/>
+        element : <App/>,
+        children : [
+            {
+                path: "about",
+                element: <About/>
+            },
+            {
+                path: "contact",
+                element:<Contact/>
+            }
+        ]
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+    {
+        path: "/registration",
+        element: <Registration/>
     }
 ]);
 
