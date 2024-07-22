16ab7f85ff15c90e75ca703fdd16fd7209f7df95
meskatabusayed <meskatabusayed@gmail.com>
2024-07-22 19:41:31 +0530

    sidebar type added

--- src/routes/admin.routes..tsx
+++ src/routes/admin.routes..tsx
@@ -9,11 +9,7 @@ import { NavLink } from "react-router-dom";
 
 
 
-type TSidebarItem = {
-    key : string;
-    label : ReactNode;
-    children ? : TSidebarItem[];
-}
+
 
 export const adminPaths = [
     {
@@ -43,27 +39,5 @@ export const adminPaths = [
     }
 ];
 
-export const adminSidebarItems = adminPaths.reduce((acc : TSidebarItem[], item) => {
-    if(item.path && item.name){
-        acc.push({
-            key : item.name,
-            label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
-        })
-    }
 
-    if(item.children){
-        acc.push({
-            key : item.name,
-            label : item.name,
-            children : item.children.map((child) => ({
-                key : child.name,
-                label : <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
-            }
-
-            ))
-        })
-    }
-
-    return acc;
-} , [])
 
---
+++ src/utils/sidebarItemsGenerator.ts
@@ -0,0 +1,31 @@
+type TSidebarItem = {
+    key : string;
+    label : ReactNode;
+    children ? : TSidebarItem[];
+}
+
+export const sidebarItemsGenerator = (items) => {
+     const sidebarItems = items.reduce((acc : TSidebarItem[], item) => {
+        if(item.path && item.name){
+            acc.push({
+                key : item.name,
+                label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
+            })
+        }
+    
+        if(item.children){
+            acc.push({
+                key : item.name,
+                label : item.name,
+                children : item.children.map((child) => ({
+                    key : child.name,
+                    label : <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
+                }
+    
+                ))
+            })
+        }
+    
+        return acc;
+    } , [])
+}
