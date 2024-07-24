c2884a7b542d076ea19ae516e954cd9672014e5e
meskatabusayed <meskatabusayed@gmail.com>
2024-07-24 23:53:56 +0530

    authentication added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,27 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
+  const {register , handleSubmit} = useForm();
+  const onSubmit = (data) => {
+    const userInfo = {
+      id : data.id ,
+      password : data.password
+    }
+  }
   return (
-    <div>
-      This is Login
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+        <Button htmlType="submit">Login</Button>
+    </form>
   )
 }
 
